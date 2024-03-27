0a317c559189cff530602634d6f9a0e57de42226
Godofdrakes <Godofdrakes@gmail.com>
2024-03-27 14:28:32 -0700

    Renamed project files

--- src/test/java/com/variableviewer/VariableViewerPluginTest.java
+++ src/test/java/com/variableviewer/VariableViewerPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.variableviewer;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class VariableViewerPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin( VariableViewerPlugin.class);
 		RuneLite.main(args);
 	}
 }
